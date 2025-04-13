bdbfd72e722ff50816481adc1db90f5c9a79f17d
magzumAlmat <almat.magzum@gmail.com>
2025-04-13 16:57:47 +0500

    13april

--- app/store/models/Product.js
+++ app/store/models/Product.js
@@ -1,52 +1,31 @@
-// src/models/Product.js
-'use strict';
-const { Model } = require('sequelize');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../../config/db'); // обновлено
 
-module.exports = (sequelize, DataTypes) => {
-  class Product extends Model {
-    static associate(models) {
-      Product.hasMany(models.ProductImage, { foreignKey: 'productId', as: 'images' });
-      Product.belongsToMany(models.Category, {
-        through: models.ProductCategory,
-        foreignKey: 'productId',
-        otherKey: 'categoryId',
-        as: 'categories',
-      });
-    }
-  }
-  Product.init(
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      volume: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      features: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-      },
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
-      },
-    },
-    {
-      sequelize,
-      modelName: 'Product',
-      tableName: 'products',
-    }
-  );
-  return Product;
-};
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+}, {
+  tableName: 'products',
+  timestamps: true,
+});
+
+module.exports = Product;
--- app/store/models/index.js
+++ app/store/models/index.js
@@ -1,21 +1,15 @@
-// src/models/index.js
+const Sequelize = require('sequelize');
+const sequelize = require('../../../config/db');
 const Product = require('./Product');
 const ProductImage = require('./ProductImage');
-const Category = require('./Category');
-const ProductCategory = require('./ProductCategory');
 
-// Связь один-ко-многим: один товар имеет много изображений
+// Определение связей
 Product.hasMany(ProductImage, { foreignKey: 'productId', as: 'images' });
-ProductImage.belongsTo(Product, { foreignKey: 'productId' });
-
-// Связь многие-ко-многим для категорий (из предыдущего ответа)
-Product.belongsToMany(Category, { through: ProductCategory, foreignKey: 'productId' });
-Category.belongsToMany(Product, { through: ProductCategory, foreignKey: 'categoryId' });
+ProductImage.belongsTo(Product, { foreignKey: 'productId', as: 'product' });
 
 module.exports = {
+  sequelize,
+  Sequelize,
   Product,
   ProductImage,
-  Category,
-  ProductCategory,
-  sequelize,
 };
